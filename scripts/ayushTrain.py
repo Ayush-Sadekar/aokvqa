d46637c1712ce2b2d037d98b3ef1b5a3e40d2e8c
Ayush-Sadekar <ayushs@vt.edu>
2025-04-12 16:14:18 -0400

    Update ayushTrain.py

--- scripts/ayushTrain.py
+++ scripts/ayushTrain.py
@@ -5,12 +5,10 @@ import argparse
 import pathlib
 
 import torch
-import clip
+#import clip
 import pandas as pd
 
 
-from load_aokvqa import load_aokvqa
-
 dataset = pd.read_json("/Users/ayush/Desktop/Multimodal_Project/datasets/aokvqa/aokvqa_v1p0_train.json")
 
-dataset.head
+print(dataset.head())
